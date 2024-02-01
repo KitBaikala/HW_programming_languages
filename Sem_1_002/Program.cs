81eeece6917ebc910a2cdaa6e8d11602e71f708a
Andrei <andrei.yakimyuk>
2024-02-01 16:39:00 +0800

    решение задачи 2

--- Sem_1_002/Program.cs
+++ Sem_1_002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// семинар 1 задача 2
+// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит,
+// является ли число чётным (делится ли оно на два без остатка).
+
+int number;                             
+    Console.WriteLine("введите число: ");
+    number = Convert.ToInt32(Console.ReadLine()); 
+    
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("четное");    
+    }
+    else
+    {
+        Console.WriteLine("нечетное");
+    }
+
+//    решение верное
+// ответ из автотеста
+// if ((number % 2) == 0)
+//        {
+//            Console.WriteLine($"четное");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"нечетное");
+//        }
+
+
