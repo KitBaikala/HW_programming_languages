84cc22b61283e1aaeea22fc0eac3a349a1f2e9b1
Andrei <andrei.yakimyuk>
2024-02-01 18:03:54 +0800

    решение задачи 4

--- Sem_1_003/Program.cs
+++ Sem_1_003/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// задача 4 семинар 1
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+
+while(count <= number)
+
+    if(count % 2 ==0) 
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+else
+{
+     count++;
+}
+
+
+// задача решена верно
+// решение от автотеста
+
+//for (int count = 2; count <= number; count += 2)
+//        {
+//            Console.Write($"{count} ");
+//        }
+
