58092df56c75eab357e324e7092a36dde694ee01
Andrei <andrei.yakimyuk>
2024-02-01 15:41:52 +0800

    решение задачи 1

--- Sem_1_001/Program.cs
+++ Sem_1_001/Program.cs
@@ -1,3 +1,52 @@
 ﻿// Семинар 1, задача 1
 
 
+ // Console.Clear();
+ // Console.Write("Введите 1-ое число: ");
+ // int n = int.Parse(Console.ReadLine()!);
+ // Console.Write("Введите 2-ое число: ");
+ // int m = int.Parse(Console.ReadLine()!);
+ // if (n > m){
+ //    Console.WriteLine($"{n} больше {m}");
+ // }
+ // else if (n < m)
+ //    Console.WriteLine($"{m} больше {n}");
+ // else
+ //    Console.WriteLine("Числа равны");
+
+Console.Clear();
+int firstNumber = 5;
+int secondNumber = 6;
+if (firstNumber > secondNumber){
+    Console.WriteLine($"первое число больше ");
+ }
+ else if (firstNumber < secondNumber) 
+    Console.WriteLine($"второе число больше ");
+ else
+    Console.WriteLine("числа равны");
+
+    // решение первой задачи.
+
+//    if (firstNumber > secondNumber){
+//    Console.WriteLine($"первое число больше ");
+// }
+// else if (firstNumber < secondNumber) 
+//    Console.WriteLine($"второе число больше ");
+// else
+//    Console.WriteLine("числа равны");
+//
+//
+// ответ из автотеста:
+// if (firstNumber == secondNumber)
+//        {
+//            Console.WriteLine($"числа равны");
+//        }
+//        if (firstNumber > secondNumber)
+//        {
+//            Console.WriteLine($"первое число больше`");
+//        }
+//        if (firstNumber < secondNumber)
+//        {
+//            Console.WriteLine($"второе число больше");
+
+
